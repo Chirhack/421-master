617fcf4bf254fdbb43f8e0115bec7bbe6dcf0336
Chirhack <reyo38@gmail.com>
2026-03-22 17:35:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = '421-master-v1.2';
+const ASSETS = [
+  './',
+  './index.html',
+  './dice.wav',
+  './clink.mp3',
+  './victory.mp3',
+  './loss.mp3',
+  './manifest.json'
+];
+
+// Installation : on met les fichiers en cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activation : on nettoie les vieux caches si la version change
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache (pour avoir toujours la dernière version si possible)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
